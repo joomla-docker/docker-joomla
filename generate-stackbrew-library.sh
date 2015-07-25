966275ada2148e343a68c8c03870f11cc7f5b89c
Michael Babker <mbabker@flbab.com>
2015-07-25 14:42:58 -0400

    Initial working build of Joomla using a Docker image

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+url='git://github.com/joomla/docker-joomla'
+
+echo '# maintainer: Michael Babker <michael.babker@joomla.org> (@mbabker)'
+
+defaultVariant='apache'
+
+for variant in apache fpm; do
+	commit="$(git log -1 --format='format:%H' -- "$variant")"
+	fullVersion="$(grep -m1 'ENV JOOMLA_VERSION ' "$variant/Dockerfile" | cut -d' ' -f3)"
+
+	versionAliases=()
+	while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+		versionAliases+=( $fullVersion-$variant )
+		if [ "$variant" = "$defaultVariant" ]; then
+			versionAliases+=( $fullVersion )
+		fi
+		fullVersion="${fullVersion%.*}"
+	done
+	versionAliases+=( $fullVersion-$variant $variant )
+	if [ "$variant" = "$defaultVariant" ]; then
+		versionAliases+=( $fullVersion latest )
+	fi
+
+	echo
+	for va in "${versionAliases[@]}"; do
+		echo "$va: ${url}@${commit} $variant"
+	done
+done
---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# TODO - We (Joomla) need to expose an alternate API to checking the latest version other than our XML files
+current="$(curl -A 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36' -sSL 'http://developer.joomla.org/latest_version.json' | sed -r 's/^.*"current":"([^"]+)".*$/\1/')"
+
+upstream="$current"
+if [[ "$current" != *.*.* ]]; then
+	# turn "3.4" into "3.4.0"
+	current+='.0'
+fi
+
+# We're putting a lot of trust in this process, once Joomla has an exposed API to query the SHA1 use that instead
+wget -O joomla.zip https://github.com/joomla/joomla-cms/releases/download/$upstream/Joomla_$upstream-Stable-Full_Package.zip
+sha1="$(sha1sum joomla.zip | sed -r 's/ .*//')"
+
+for variant in apache fpm; do
+	(
+		set -x
+
+		sed -ri '
+			s/^(ENV JOOMLA_VERSION) .*/\1 '"$current"'/;
+			s/^(ENV JOOMLA_UPSTREAM_VERSION) .*/\1 '"$upstream"'/;
+			s/^(ENV JOOMLA_SHA1) .*/\1 '"$sha1"'/;
+		' "$variant/Dockerfile"
+
+        # To make management easier, we use these files for all variants
+		cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
+		cp joomla-htaccess.txt "$variant/joomla-htaccess.txt"
+		cp makedb.php "$variant/makedb.php"
+	)
+done
