b365e4a9d8c017e1bdcd666d309a69eaea07dfeb
Michael Babker <mbabker@flbab.com>
2016-08-16 07:14:03 -0500

    Convert "generate-stackbrew-library.sh" to output the new 2822-based format

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,33 +1,74 @@
 #!/bin/bash
-set -e
+set -eu
 
+self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-url='https://github.com/joomla/docker-joomla'
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
 
-echo '# maintainer: Michael Babker <michael.babker@joomla.org> (@mbabker)'
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		fileCommit \
+			Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						print $i
+					}
+				}
+			')
+	)
+}
 
-defaultVariant='apache'
+cat <<-EOH
+# this file is generated via https://github.com/joomla/docker-joomla/blob/$(fileCommit "$self")/$self
+
+Maintainers: Michael Babker <michael.babker@joomla.org> (@mbabker)
+GitRepo: https://github.com/joomla/docker-joomla.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
 
 for variant in apache apache-php7 fpm fpm-php7; do
-	commit="$(git log -1 --format='format:%H' -- "$variant")"
-	fullVersion="$(grep -m1 'ENV JOOMLA_VERSION ' "$variant/Dockerfile" | cut -d' ' -f3)"
+	commit="$(dirCommit "$variant")"
+
+	fullVersion="$(git show "$commit":"$variant/Dockerfile" | awk '$1 == "ENV" && $2 == "JOOMLA_VERSION" { print $3; exit }')"
+	if [[ "$fullVersion" != *.*.* && "$fullVersion" == *.* ]]; then
+		fullVersion+='.0'
+	fi
 
 	versionAliases=()
 	while [ "${fullVersion%.*}" != "$fullVersion" ]; do
-		versionAliases+=( $fullVersion-$variant )
-		if [ "$variant" = "$defaultVariant" ]; then
-			versionAliases+=( $fullVersion )
-		fi
+		versionAliases+=( $fullVersion )
 		fullVersion="${fullVersion%.*}"
 	done
-	versionAliases+=( $fullVersion-$variant $variant )
-	if [ "$variant" = "$defaultVariant" ]; then
-		versionAliases+=( $fullVersion latest )
+	versionAliases+=(
+		$fullVersion
+		latest
+	)
+
+	variantAliases=( "${versionAliases[@]/%/-$variant}" )
+	variantAliases=( "${variantAliases[@]//latest-/}" )
+
+	if [ "$variant" = 'apache' ]; then
+		variantAliases+=( "${versionAliases[@]}" )
 	fi
 
 	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $variant"
-	done
+	cat <<-EOE
+		Tags: $(join ', ' "${variantAliases[@]}")
+		GitCommit: $commit
+		Directory: $variant
+	EOE
 done
