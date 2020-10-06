2c5c8045302b0cfe821508595eb19b87070a459c
J0WI <J0WI@users.noreply.github.com>
2020-10-06 05:28:02 +0000

    APCu 5.1.19 (#105)

--- update.sh
+++ update.sh
@@ -29,7 +29,7 @@ declare -A variantBases=(
 	[fpm-alpine]='alpine'
 )
 declare -A pecl_versions=(
-	[php7-APCu]='5.1.18'
+	[php7-APCu]='5.1.19'
 	[php7-memcached]='3.1.5'
 	[php7-redis]='4.3.0'
 	[php7-mcrypt]='1.0.3'
